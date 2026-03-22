a790c1667f51b481eaf8abc188903ac6da39693c
tony weston <tonyw@totspics.com>
2026-03-22 08:53:32 +0000

    Refactored, tidied up

--- examples/TouchPrint/TouchPrint.ino
+++ examples/TouchPrint/TouchPrint.ino
@@ -1,30 +1,29 @@
 #include "TAMC_GT911.h"
+#include "Wire.h"
 
-#define TOUCH_SDA  4
-#define TOUCH_SCL  5
-#define TOUCH_INT 25
-#define TOUCH_RST 26
-#define TOUCH_WIDTH  320
-#define TOUCH_HEIGHT 240
+#define TOUCH_SDA  39
+#define TOUCH_SCL  40
+#define TOUCH_INT  3
+#define TOUCH_RST  26
 
-TAMC_GT911 tp = TAMC_GT911(TOUCH_SDA, TOUCH_SCL, TOUCH_INT, TOUCH_RST, TOUCH_WIDTH, TOUCH_HEIGHT);
+TAMC_GT911 tp;
+
+TwoWire wire(0);
 
 void setup() {
+  wire.begin(TOUCH_SDA, TOUCH_SCL);
   Serial.begin(115200);
-  Serial.println("TAMC_GT911 Example: Ready");
-  tp.begin();
-  tp.setRotation(ROTATION_NORMAL);
+  Serial.println("GT911 Lite Example: Ready");
+  tp.begin(&wire);
 }
 
 void loop() {
-  tp.read();
-  if (tp.isTouched){
-    for (int i=0; i<tp.touches; i++){
-      Serial.print("Touch ");Serial.print(i+1);Serial.print(": ");;
-      Serial.print("  x: ");Serial.print(tp.points[i].x);
-      Serial.print("  y: ");Serial.print(tp.points[i].y);
-      Serial.print("  size: ");Serial.println(tp.points[i].size);
-      Serial.println(' ');
+  if (tp.read()) {
+    for (int i = 0; i < tp.touches; i++) {
+      Serial.print("Touch "); Serial.print(i + 1); Serial.print(": ");
+      Serial.print("  x: ");    Serial.print(tp.points[i].x);
+      Serial.print("  y: ");    Serial.print(tp.points[i].y);
+      Serial.print("  size: "); Serial.println(tp.points[i].size);
     }
   }
 }
